20162c68d499c3ed8b47666233622a27f86335ef
gigabyte_tech <gigabyte_tech@ukr.net>
2021-07-20 17:14:54 +0300

    move all logic from controller to model

--- routes/web.php
+++ routes/web.php
@@ -13,27 +13,34 @@
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function (){
-    Route::get('/', function () { return redirect()->route('users.all');});
-    Route::get('/users', 'UsersController@all')->name('users.all');
-    Route::get('/user/profile/{id}', 'UsersController@profile')->name('users.profile');
-});
+Route::middleware(['auth'])->get('/', function () { return redirect()->route('users.all');});
 
-Route::group(['middleware' => ['auth', 'IsAdminOrOwner']], function () {
-    Route::get('/user/create', 'UsersController@create')->name('users.create');
-    Route::post('/user/store', 'UsersController@store')->name('users.store');
+Route::name('users.')
+    ->prefix('user')
+    ->group(function () {
+        Route::middleware(['auth'])
+            ->group(function () {
+                Route::get('', 'UsersController@all')->name('all');
+                Route::get('profile/{id}', 'UsersController@profile')->name('profile');
+                Route::middleware(['IsAdminOrOwner'])
+                    ->group(function () {
+                        Route::get('create', 'UsersController@create')->name('create');
+                        Route::post('store', 'UsersController@store')->name('store');
 
-    Route::get('/user/edit/{id}', 'UsersController@editInfo')->name('users.edit_info');
-    Route::post('/user/edit/{id}', 'UsersController@updateInfo')->name('users.update_info');
+                        Route::get('edit/{id}', 'UsersController@editInfo')->name('edit_info');
+                        Route::post('edit/{id}', 'UsersController@updateInfo')->name('update_info');
 
-    Route::get('/user/security/{id}', 'UsersController@editSecurity')->name('users.edit_security');
-    Route::post('/user/security/{id}', 'UsersController@updateSecurity')->name('users.update_security');
+                        Route::get('security/{id}', 'UsersController@editSecurity')->name('edit_security');
+                        Route::post('security/{id}', 'UsersController@updateSecurity')->name('update_security');
 
-    Route::get('/user/status/{id}', 'UsersController@editStatus')->name('users.edit_status');
-    Route::post('/user/status/{id}', 'UsersController@updateStatus')->name('users.update_status');
+                        Route::get('status/{id}', 'UsersController@editStatus')->name('edit_status');
+                        Route::post('status/{id}', 'UsersController@updateStatus')->name('update_status');
 
-    Route::get('/user/avatar/{id}', 'UsersController@editAvatar')->name('users.edit_avatar');
-    Route::post('/user/avatar/{id}', 'UsersController@updateAvatar')->name('users.update_avatar');
+                        Route::get('avatar/{id}', 'UsersController@editAvatar')->name('edit_avatar');
+                        Route::post('avatar/{id}', 'UsersController@updateAvatar')->name('update_avatar');
 
-    Route::get('/user/delete/{id}', 'UsersController@delete')->name('users.delete');
-});
+                        Route::get('delete/{id}', 'UsersController@delete')->name('delete');
+
+                    });
+            });
+    });
